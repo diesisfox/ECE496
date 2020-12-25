5498ea4fc9a9abdf432b99016a6dc85f82fecf8e
jjpdn <51329642+jjpdn@users.noreply.github.com>
2020-12-25 12:30:16 -0500

    inserted python interpreter, allowing user to run wrriten python code in _console_ and receiving output. No input allowed.

--- my-electron-app/windows/main_window/main_window.js
+++ my-electron-app/windows/main_window/main_window.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, Menu, ipcMain} = require('electron')
 
+const {PythonShell} = require('python-shell')
+
 const file_manager = require('../file_management/file_manager')
 
 let win = null;
@@ -65,11 +67,33 @@ function createMainWindow () {
   createMenu();
 }
 
-ipcMain.on('console-message', (event,arg) => {
-  console.log(arg);
+// -------------- PYTHON SHELL --------------
+
+ipcMain.on('get-python-version', (event,arg)=>{
+  const version = PythonShell.getVersionSync()
+  event.returnValue = version
+})
+
+/*
+Idea: write python script with code that imports all required libraries and etc. 
+Write controller script.
+Create a temp copy of it and append user code. 
+Reload the temp copy in the controller script and create pipe between two scripts, run the user function. 
+Pass new state of systems to controller to do whatever with. 
+ */
+ipcMain.on('console-input-reading', (event,arg) => {
+  let python_instance = PythonShell.runString(arg, null, function (err) {
+    if (err) {
+      console.log(err)
+      event.reply('console-message', err)
+    }
+  })
+  python_instance.on('message', function (message) {
+    event.reply('console-message', message)
+  })
 })
 
   
 module.exports = {
-    createMainWindow,
+  createMainWindow,
 }
--- my-electron-app/windows/main_window/python-shell-terminal.js
+++ my-electron-app/windows/main_window/python-shell-terminal.js
@@ -3,12 +3,18 @@ const {ipcRenderer} = require('electron')
 var input_box = document.getElementById("console-input")
 var text_box = document.getElementById("console-text")
 
+text_box.textContent = ipcRenderer.sendSync('get-python-version', '')
+
 const addToTextBox = function(e){
     if (e.key === 'Enter'){
-        ipcRenderer.send('console-message', input_box.value);
+        ipcRenderer.send('console-input-reading', input_box.value);
         text_box.textContent = text_box.textContent + "\r\n>>> " + input_box.value;
         input_box.value = ""
     }
 }
+
+ipcRenderer.on('console-message', (event,arg) => {
+    text_box.textContent = text_box.textContent + "\r\n" + arg
+})
 
 input_box.addEventListener('keydown', addToTextBox)
