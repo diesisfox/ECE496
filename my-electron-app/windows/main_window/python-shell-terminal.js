cb47895bb74690a34885706c92bee1bafe1a0e78
jjpdn <51329642+jjpdn@users.noreply.github.com>
2020-12-25 12:43:03 -0500

    added autoscroll for console text and instructions for clear

--- my-electron-app/windows/main_window/python-shell-terminal.js
+++ my-electron-app/windows/main_window/python-shell-terminal.js
@@ -2,9 +2,11 @@ const {ipcRenderer} = require('electron')
 
 var input_box = document.getElementById("console-input")
 var text_box = document.getElementById("console-text")
+var text_wrapper = document.getElementById("console-text-wrapper")
 
 // Start console with python version displayed.
-text_box.textContent = ipcRenderer.sendSync('get-python-version', '')
+text_box.textContent = ipcRenderer.sendSync('get-python-version', '') + 
+    "\r\nType 'clear' without apostrophes to clear."
 
 // Deal with console inputs and outputs. 
 const addToTextBox = function(e){
@@ -16,12 +18,14 @@ const addToTextBox = function(e){
             ipcRenderer.send('console-input-reading', input_box.value);
             text_box.textContent = text_box.textContent + "\r\n>>> " + input_box.value;
             input_box.value = ""
+            text_wrapper.scrollTop = text_wrapper.scrollHeight
         }
     }
 }
 
 ipcRenderer.on('console-message', (event,arg) => {
     text_box.textContent = text_box.textContent + "\r\n" + arg
+    text_wrapper.scrollTop = text_wrapper.scrollHeight
 })
 
 input_box.addEventListener('keydown', addToTextBox)
