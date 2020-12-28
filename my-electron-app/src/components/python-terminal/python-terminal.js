3cead8e05bc89b443feded974a0fd9a8d2c5360f
jjpdn <51329642+jjpdn@users.noreply.github.com>
2020-12-28 16:39:16 -0500

    connected python interpreter; TODO: fix bug where listeners stack up if there is no console output

--- my-electron-app/src/components/python-terminal/python-terminal.js
+++ my-electron-app/src/components/python-terminal/python-terminal.js
@@ -1,24 +1,7 @@
-
 const {PythonShell} = require('python-shell')
+const child_process = require('child_process')
 
-
-function addPythonHandlers (ipcMain) {
-    ipcMain.on('get-python-version', (event,arg)=>{
-        const version = PythonShell.getVersionSync()
-        event.returnValue = version
-    })
-    ipcMain.on('console-input-reading', (event,arg) => {
-        let python_instance = PythonShell.runString(arg, null, function (err) {
-            if (err) {
-                console.log(err)
-                event.reply('console-message', err)
-            }
-        })
-        python_instance.on('message', function (message) {
-            event.reply('console-message', message)
-        })
-    })
-}
+var python_instance = null;
 
 /*
 Idea: write python script with code that imports all required libraries and etc. 
@@ -28,7 +11,23 @@ Reload the temp copy in the controller script and create pipe between two script
 Pass new state of systems to controller to do whatever with. 
 */
 function initializePythonProcess (ipcMain) {
-    addPythonHandlers(ipcMain)
+    python_instance = child_process.spawn('python', ['-i'])
+    python_instance.stdin.write("print(\"test\")\n")
+    python_instance.stdout.once('data', (data)=>{
+        console.log(data.toString())
+    })
+
+    ipcMain.on('get-python-version', (event,arg)=>{
+        const version = PythonShell.getVersionSync()
+        event.returnValue = version
+    })
+    ipcMain.on('console-input-reading', (event,arg) => {
+        python_instance.stdin.write(arg+"\n")
+
+        python_instance.stdout.once('data', function (data) {
+            event.reply('console-message', data.toString())
+        })
+    })
 }
 
 module.exports = {
