4cd4e612a849f5316bf39386e570777e9e0c14ab
jjpdn <51329642+jjpdn@users.noreply.github.com>
2020-12-28 16:43:44 -0500

    fixed error with stacking printouts from python console)

--- my-electron-app/src/components/python-terminal/python-terminal.js
+++ my-electron-app/src/components/python-terminal/python-terminal.js
@@ -3,13 +3,6 @@ const child_process = require('child_process')
 
 var python_instance = null;
 
-/*
-Idea: write python script with code that imports all required libraries and etc. 
-Write controller script.
-Create a temp copy of it and append user code. 
-Reload the temp copy in the controller script and create pipe between two scripts, run the user function. 
-Pass new state of systems to controller to do whatever with. 
-*/
 function initializePythonProcess (ipcMain) {
     python_instance = child_process.spawn('python', ['-i'])
     python_instance.stdin.write("print(\"test\")\n")
@@ -23,6 +16,8 @@ function initializePythonProcess (ipcMain) {
     })
     ipcMain.on('console-input-reading', (event,arg) => {
         python_instance.stdin.write(arg+"\n")
+
+        python_instance.stdout.removeAllListeners('data')
 
         python_instance.stdout.once('data', function (data) {
             event.reply('console-message', data.toString())
