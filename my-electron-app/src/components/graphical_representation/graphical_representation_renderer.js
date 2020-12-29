3039102a495152848b48727a9d5488a36a67e99e
jjpdn <51329642+jjpdn@users.noreply.github.com>
2020-12-29 16:36:36 -0500

    renamed graphical_representation.js, removed panning using the mouse wheel on graphical reprensentation (GR), and added ability to freely move diagram off screen in all directions

--- my-electron-app/src/components/graphical_representation/graphical_representation_renderer.js
+++ my-electron-app/src/components/graphical_representation/graphical_representation_renderer.js
@@ -8,6 +8,9 @@ const div = document.getElementById("model-diagram-display")
 var model = new mxgraph.mxGraphModel();
 var graph = new mxgraph.mxGraph(div, model);
 
+graph.setPanning(true)
+graph.setEnabled(false)
+
 var parent = graph.getDefaultParent();
 
 // Adds cells to the model in a single step
@@ -17,11 +20,22 @@ try
   var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
   var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
   var e1 = graph.insertEdge(parent, null, '', v1, v2);
-
-//   graph.setEnabled(false)
+  var v1 = graph.insertVertex(parent, null, 'Default,', 300, 250, 80, 30);
+  var v2 = graph.insertVertex(parent, null, 'Graph!', 700, 400, 80, 30);
+  var e1 = graph.insertEdge(parent, null, '', v1, v2);
 }
 finally
 {
   // Updates the display
   model.endUpdate();
+}
+
+function wipeGraphicalDisplay(){
+  model = new mxgraph.mxGraphModel();
+  graph = new mxgraph.mxGraph(div, model);
+}
+
+module.exports = {
+  wipeGraphicalDisplay,
+
 }
