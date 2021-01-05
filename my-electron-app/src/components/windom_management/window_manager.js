3f868320ecd64d9e74e3fcb2a1808ae7677700ec
jjpdn <51329642+jjpdn@users.noreply.github.com>
2021-01-04 19:42:37 -0500

    added window offset for help window

--- my-electron-app/src/components/windom_management/window_manager.js
+++ my-electron-app/src/components/windom_management/window_manager.js
@@ -1,14 +1,22 @@
 const {BrowserWindow} = require('electron')
 
-function createWindow (html_path, w, h) {
-    const win_temp = new BrowserWindow({
+function createWindow (html_path, w, h, x_offset = -1, y_offset = -1) {
+    let win_options = {
         width: w,
         height: h,
         show: false,
         webPreferences: {
         nodeIntegration: true
         }
-    })
+    }
+
+    // use offset if given, otherwise, center window
+    if (x_offset != -1 || y_offset != -1){
+        win_options.x = x_offset
+        win_options.y = y_offset
+    }
+
+    const win_temp = new BrowserWindow(win_options)
 
     // Root is CAPSTONE/my-electron-app for some reason
     win_temp.loadFile(html_path)
--- my-electron-app/src/help_window/help_window.js
+++ my-electron-app/src/help_window/help_window.js
@@ -4,7 +4,7 @@ const window_manager = require('../components/windom_management/window_manager.j
 let help_win = null
 
 function createHelpWindow (ipcMain) {
-  help_win = window_manager.createWindow('./src/help_window/help_window.html', 800, 600)
+  help_win = window_manager.createWindow('./src/help_window/help_window.html', 800, 600, 20, 20)
 
   help_win.setMenu(null)
 
