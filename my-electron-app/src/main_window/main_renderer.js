fbfe5da691d09c43479f1796dc6424b26f3ee516
jjpdn <51329642+jjpdn@users.noreply.github.com>
2021-01-13 16:39:09 -0500

    moved console css to its own file and started work on backbone implementation of graphical representation

--- my-electron-app/src/main_window/main_renderer.js
+++ my-electron-app/src/main_window/main_renderer.js
@@ -1,7 +1,7 @@
 const {ipcRenderer} = require('electron')
 
 const python_terminal_renderer = require('../components/python_terminal/python_terminal_renderer.js')
-const graphical_representation_renderer = require('../components/graphical_representation/graphical_representation_renderer.js')
+const graphical_representation_renderer = require('../components/graphical_representation_spiderweb/graphical_representation_renderer.js')
 //const graphical_toolbox_renderer = require('../components/graphical_representation/graphical_toolbox_renderer.js')
 
 // -------- HTML ELEMENTS --------
