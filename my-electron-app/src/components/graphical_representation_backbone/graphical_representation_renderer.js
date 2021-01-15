189ec918f62d6aecb2446ffed983eb9ee3d7c881
jjpdn <51329642+jjpdn@users.noreply.github.com>
2021-01-14 23:31:16 -0500

    moved files to right places, added basic testing svg

--- my-electron-app/src/components/graphical_representation_backbone/graphical_representation_renderer.js
+++ my-electron-app/src/components/graphical_representation_backbone/graphical_representation_renderer.js
@@ -1,6 +1,8 @@
 const toolbox = require("../toolbox/toolbox.js")
 const theme = require("../theme.js")
+const CONSTANTS = require("../../constants.js")
 
+const ns = "http://www.w3.org/2000/svg"
 const diagram_div = document.getElementById("model-diagram-display")
 const rect_width = 80
 const rect_height = 30
@@ -14,17 +16,51 @@ let totalNum = 1
 
 // --------- Display Methods ---------
 
+function createHtmlModuleRep (){
+  var wrapper = document.createElement('div')
+  wrapper.style.width = rect_width
+  wrapper.style.height = rect_height+10
+  diagram_div.appendChild(wrapper)
+
+  var text = document.createElement('p')
+  text.style.textAlign = 'center'
+  text.textContent = '||'
+  wrapper.appendChild(text)
+
+  var module = document.createElement('div')
+  module.className = 'module-box-main'
+  module.style.width = rect_width
+  module.style.height = rect_height
+  wrapper.appendChild(module)
+
+  module.style.backgroundColor = theme.light_blue_color
+}
+
+function createSVGModuleRep (ipcRenderer) {
+  
+  let svg_diagram = document.createElementNS(ns, "svg")
+  svg_diagram.setAttributeNS(null, "width", '100%')
+  svg_diagram.setAttributeNS(null, "height", '100%')
+  svg_diagram.style.backgroundColor = theme.light_blue_color
+  diagram_div.appendChild(svg_diagram)
+
+  let i = 0
+  for (i = 0; i < totalNum; i++){
+    ipcRenderer.send('debug', '1')
+    let element = document.createElementNS(ns, 'circle')
+    element.setAttribute("cx",100);
+    element.setAttribute("cy",100);
+    element.setAttribute("r",100);
+    element.setAttribute("fill",'yellow');
+    element.style.backgroundColor = 'white'
+    svg_diagram.appendChild(element)
+    ipcRenderer.send('debug', '2')
+  }
+}
+
 // unfinished, needs JSON schema for completion
-function displayJSON(json){
-    let i = 0
-    for (i = 0; i < totalNum; i++){
-        var module = document.createElement("div")
-        module.className = 'module-box-main'
-
-        module.style.backgroundColor = theme.light_blue_color
-
-        diagram_div.appendChild(module)
-    }
+function displayJSON(ipcRenderer,json){
+  createSVGModuleRep(ipcRenderer)
 
 
 }
@@ -39,7 +75,9 @@ function init (ipcRenderer) {
 
   //displayTests()
   //wipeGraphicalDisplay()
-    displayJSON("blab")
+    displayJSON(ipcRenderer, "blab")
+
+    ipcRenderer.send('debug', 'finished')
 }
 
 
