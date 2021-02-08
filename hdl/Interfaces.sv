b139b0a2cc899cd3128ff55a6418dda327953b4f
James Liu <james.zhi.liu@gmail.com>
2021-02-08 02:50:54 -0500

    added byteEn and sim utils to interfaces.sv

--- hdl/Interfaces.sv
+++ hdl/Interfaces.sv
@@ -30,6 +30,26 @@ interface Simple_Mem_IF ();
     logic           rd_valid;
     logic           rd_ready;
 
+    task CWrite(input logic [31:0] addr, input logic [31:0] data, input int cycleTime);
+        wr_addr = addr;
+        wr_data = data;
+        wr_byteEn = 4'hf;
+        wr_valid = '1;
+        do #cycleTime;
+        while(wr_ready == '0);
+        wr_valid = '0;
+    endtask
+
+    task CRead(input logic [31:0] addr, output logic [31:0] data, input int cycleTime);
+        rd_addr = addr;
+        rd_byteEn = 4'hf;
+        rd_valid = '1;
+        do #cycleTime;
+        while(rd_ready == '0);
+        data = rd_data;
+        rd_valid = '0;
+    endtask
+
     modport COWI (
         input clock, reset_n,
 
@@ -57,7 +77,7 @@ interface Simple_Mem_IF ();
 
         output rd_addr,
         output rd_valid,
-        output wr_byteEn,
+        output rd_byteEn,
         input rd_data,
         input rd_ready
 	);
